143dae647309425ab7475cc887384a0bed83df4c
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-08-31 20:02:41 +0700

    Setup config loader

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"context"
+	"github.com/etwicaksono/go-hexagonal-architecture/config"
+	"github.com/etwicaksono/go-hexagonal-architecture/internal/adapter/core/entity"
+	"log/slog"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	shutdown := make(chan error, 1)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	_, err := config.LoadConfig()
+	if err != nil {
+		slog.ErrorContext(ctx, "Failed to load cfg", slog.String(entity.Error, err.Error()))
+	}
+
+	select {
+	case err = <-shutdown:
+		// Wait throw error
+		return
+	case <-ctx.Done():
+		// Wait for first CTRL+C.
+		// Stop receiving signal notifications as soon as possible.
+		stop()
+	}
+}
--- config/config.go
+++ config/config.go
@@ -1,1 +1,59 @@
 package config
+
+import (
+	"github.com/spf13/viper"
+	"path/filepath"
+	"runtime"
+	"time"
+)
+
+type Config struct {
+	App AppConfig
+}
+
+type AppConfig struct {
+	Env             string
+	RestHost        string
+	RestPort        int
+	GrpcHost        string
+	GrpcPort        int
+	ReadTimeout     time.Duration
+	WriteTimeout    time.Duration
+	GracefulTimeout time.Duration
+	Name            string
+	Version         string
+	Host            string
+}
+
+func LoadConfig() (Config, error) {
+	_, b, _, _ := runtime.Caller(0)
+
+	// Root folder of this project
+	projectRoot := filepath.Join(filepath.Dir(b), "../")
+	vpr := viper.New()
+
+	vpr.AddConfigPath(projectRoot)
+	vpr.SetConfigFile(".env")
+	vpr.AutomaticEnv()
+
+	err := vpr.ReadInConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return Config{
+		App: AppConfig{
+			Env:             vpr.GetString("APP_ENV"),
+			RestHost:        vpr.GetString("APP_REST_NATIVE_HOST"),
+			RestPort:        vpr.GetInt("APP_REST_NATIVE_PORT"),
+			GrpcHost:        vpr.GetString("APP_GRPC_HOST"),
+			GrpcPort:        vpr.GetInt("APP_GRPC_PORT"),
+			ReadTimeout:     vpr.GetDuration("APP_READ_TIMEOUT"),
+			WriteTimeout:    vpr.GetDuration("APP_WRITE_TIMEOUT"),
+			GracefulTimeout: vpr.GetDuration("APP_GRACEFUL_TIMEOUT"),
+			Name:            vpr.GetString("APP_NAME"),
+			Version:         vpr.GetString("APP_VERSION"),
+			Host:            vpr.GetString("APP_HOST"),
+		},
+	}, nil
+}
