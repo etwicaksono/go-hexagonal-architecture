797d485767f6c400c60a95477dace07cb63ae50c
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-09-06 02:32:31 +0700

    Use grpc interface as interface definition

--- internal/ports/primary/grpc/grpc.go
+++ internal/ports/primary/grpc/grpc.go
@@ -1,16 +1,10 @@
 package grpc
 
 import (
-	"context"
-
 	"github.com/etwicaksono/public-proto/gen/example"
 )
 
 type GrpcHandlerInterface interface {
 	Run() error
-
-	/*
-		ExampleGrpcHandlerInterface
-	*/
-	GetExampleMessage(ctx context.Context, in *example.ExampleRequest) (*example.ExampleResponse, error)
+	example.ExampleServiceServer
 }
